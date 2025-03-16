6dc85627b29f88ef16192a7dc84554c22f0fbcd7
Musakalamz <ogunsolumusa@gmail.com>
2025-03-16 16:57:41 +0100

    Displaying the SignIn and SignOut

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,26 @@ import Link from "next/link";
 export default async function Header() {
   const session = await auth();
 
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ""} />;
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <Button type="submit" color="secondary" variant="bordered">
+            {" "}
+            Sign In
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button type="submit" color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </>
+    );
+  }
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -25,11 +45,7 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          {session?.user ? <div>Signed</div> : <div>Signed out</div>}
-        </NavbarItem>
-      </NavbarContent>
+      <NavbarContent justify="end">{authContent}</NavbarContent>
     </Navbar>
   );
 }
