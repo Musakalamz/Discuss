529bb3eb172a29abab8104ed6439e59c320936c5
Musakalamz <ogunsolumusa@gmail.com>
2025-03-16 18:07:22 +0100

    Static Caching while using Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import { signIn, signOut } from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-import { Button } from "@nextui-org/react";
-
-export default async function Home() {
-  const session = await auth();
-
-  return (
-    <div>
-      <form action={signIn}>
-        <Button type="submit"> Sign In </Button>
-      </form>
-
-      <form action={signOut}>
-        <Button type="submit"> Sign Out </Button>
-      </form>
-
-      {session?.user ? (
-        <div>signed in {JSON.stringify(session.user)} </div>
-      ) : (
-        <div>signed out</div>
-      )}
-
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div>HomePage</div>;
 }
