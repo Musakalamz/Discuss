83cd62776790aff26490583a87dbb4c600e5ea9d
Musakalamz <ogunsolumusa@gmail.com>
2025-03-26 16:08:48 +0100

    Scaffolding [slug] page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-  return <div>Topic Show</div>;
+interface TopicShowPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function page({ params }: TopicShowPageProps) {
+  const { slug } = await params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="font-bold text-2xl mb-2">{slug}</h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
