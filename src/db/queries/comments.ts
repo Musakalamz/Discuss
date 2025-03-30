708d38ec3ab4c8b9e87f2f81ba6c412257a4b4a7
Musakalamz <ogunsolumusa@gmail.com>
2025-03-30 15:13:06 +0100

    Deduplicating request with CACHE (Request Memoization without using FETCH function )

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,24 +1,29 @@
 import type { Comment } from "@prisma/client";
 import { db } from "..";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export async function fetchCommentByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("Introducing duplicate queries");
+// export async function fetchCommentByPostId(
+//   postId: string
+// ): Promise<CommentWithAuthor[]> {
 
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("Introducing duplicate queries");
+
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
