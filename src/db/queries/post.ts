a52ec68cacb92131eb8fbf55fade7f5796432add
Musakalamz <ogunsolumusa@gmail.com>
2025-03-30 16:30:16 +0100

    Top post on the Homepage

--- src/db/queries/post.ts
+++ src/db/queries/post.ts
@@ -24,3 +24,19 @@ export function fetchPostByTopicSlug(slug: string): Promise<PostWithData[]> {
     },
   });
 }
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: {
+      comments: {
+        _count: "desc",
+      },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comment: true } },
+    },
+    take: 5,
+  });
+}
